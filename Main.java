eb5aecb9fffd9b4098caa740171abdaac4a608b9
楓 悠斗 <104414967+3-I-KaedeYuto@users.noreply.github.com>
2022-11-17 20:21:12 +0000

    update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import java.util.function.DoubleUnaryOperator;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    DoubleUnaryOperator f = x -> {
+        double res = 1;
+        for(int i = 0; i < 5; i++) res *= x;
+        return res;
+    };
+    double ans = DefiniteIntegral.get(f, 0, 1, 1000000);
+    System.out.println(ans);
   }
 }
